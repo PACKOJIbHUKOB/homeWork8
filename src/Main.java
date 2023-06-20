56718b293d40169cae65e496e5e25ecd09c2d36a
PACKOJIbHUKOB <jordn-5@yandex.ru>
2023-06-20 17:02:46 +0300

    Домашнее задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,91 +1,100 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {   task1();
+    public static void main(String[] args) {
+        task1();
         task2();
         task3();
         task4();
-        task5();
-        task6();
-        task7();
-        task8();}
+    }
+
     private static void task1() {
         System.out.println("задача 1");
-        int[] weight=new int[3];
-        weight[0]=1;
-        weight[1]=2;
-        weight[2]=3;
-        for (int i = 0; i < 3; i++) {
-            System.out.println(weight[i]);
-        }
-        float[] fractionalNumber=new float[]{1.57f, 7.654f, 9.986f};
-        for (int n = 0; n < 3; n++) {
-            System.out.println(fractionalNumber[n]);
-        }
-        double[] random=new double[]{153,247,854.26,1243.01,6578.12};
-        for (int a = 0; a < random.length; a++) {
-            System.out.println(random[a]);
-        }
+        System.out.println("<массив 1>,");
+        int[] weight = new int[3];
+        weight[0] = 1;
+        weight[1] = 2;
+        weight[2] = 3;
+        System.out.println(Arrays.toString(weight));
+        System.out.println("<массив 2>");
+        float[] fractionalNumber = {1.57f, 7.654f, 9.986f};
+        System.out.println(Arrays.toString(fractionalNumber));
+        System.out.println("<массив 3>");
+        double[] random = {153, 247, 854.26, 1243.01, 6578.12};
+        System.out.println(Arrays.toString(random));
     }
+
     private static void task2() {
         System.out.println("задача 2");
-        int[] weight={1,2,3};
-
-        for (int i = 0; i < 3; i++) {
-            System.out.print(weight[i]+", ");
+        int[] weight = {1, 2, 3};
+        for (int i = 0; i < weight.length; i++) {
+            if (i == weight.length - 1) {
+                System.out.print(weight[i]);
+                break;
+            }
+            System.out.print(weight[i] + ", ");
         }
         System.out.println();
-        float[] fractionalNumber={1.57f, 7.654f, 9.986f};
-        for (int n = 0; n < 3; n++) {
-            System.out.print(fractionalNumber[n]+", ");
+        float[] fractionalNumber = {1.57f, 7.654f, 9.986f};
+        for (int n = 0; n < fractionalNumber.length; n++) {
+            if (n == fractionalNumber.length - 1) {
+                System.out.print(fractionalNumber[n]);
+                break;
+            }
+            System.out.print(fractionalNumber[n] + ", ");
         }
         System.out.println();
-        double[] random={153,247,854.26,1243.01,6578.12};
-        for(int a = 0; a < random.length; a++) {
-            System.out.print(random[a]+", ");
+        double[] random = {153, 247, 854.26, 1243.01, 6578.12};
+        for (int a = 0; a < random.length; a++) {
+            if (a == random.length - 1) {
+                System.out.print(random[a]);
+                break;
+            }
+            System.out.print(random[a] + ", ");
         }
         System.out.println();
     }
+
     private static void task3() {
         System.out.println("задача 3");
-        int[] weight=new int[3];
-        weight[0]=1;
-        weight[1]=2;
-        weight[2]=3;
+        int[] weight = {1, 2, 3};
         for (int i = 2; i >= 0; i--) {
-            System.out.print(weight[i]+", ");
+            if (i == 0) {
+                System.out.print(weight[i]);
+                break;
+            }
+            System.out.print(weight[i] + ", ");
         }
         System.out.println();
-        float[] fractionalNumber=new float[]{1.57f, 7.654f, 9.986f};
-        for (int n = 2; n >=0; n--) {
-            System.out.print(fractionalNumber[n]+", ");
+        float[] fractionalNumber = new float[]{1.57f, 7.654f, 9.986f};
+        for (int n = fractionalNumber.length - 1; n >= 0; n--) {
+            if (n == 0) {
+                System.out.print(fractionalNumber[n]);
+                break;
+            }
+            System.out.print(fractionalNumber[n] + ", ");
         }
         System.out.println();
-        double[] random={153,247,854.26,1243.01,6578.12};
-        for (int a =4; a >=0; a--) {
-            System.out.print(random[a]+", ");
+        double[] random = {153, 247, 854.26, 1243.01, 6578.12};
+        for (int a = 4; a >= 0; a--) {
+            if (a == 0) {
+                System.out.print(random[a]);
+                break;
+            }
+            System.out.print(random[a] + ", ");
         }
         System.out.println();
 
     }
+
     private static void task4() {
         System.out.println("задача 4");
-        int[] weight={1,2,3};
-        for (int i = 0; i <=2; i++) {
-            if (i % 2==0){
+        int[] weight = {1, 2, 3};
+        for (int i = 0; i <= 2; i++) {
+            if (i % 2 == 0) {
                 weight[i]++;
                 System.out.println(weight[i]);
             }
         }
     }
-
-    private static void task5() {
-        System.out.println("задача 5");}
-
-    private static void task6() {
-        System.out.println("задача 6");}
-
-    private static void task7() {
-        System.out.println("задача 7");}
-
-    private static void task8() {
-        System.out.println("задача 8");}
 }
